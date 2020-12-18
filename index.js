4eb493b051a5c3a6af1e40300b3578f6a53b220a
Nate Montgomery <kilikalbright@hotmail.com>
2020-12-18 16:47:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function takeANumber(lineOfPeople, newName) {
+  lineOfPeople.push(newName)
+
+  return `Welcome, ${newName}. You are number ${lineOfPeople.length} in line.`
+}
+
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine == null){
+    return 
+  }
+}
+
+
+
+
+
+
+takeANumber();
+nowServing();
